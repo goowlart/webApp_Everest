735e340c115c36b5662921b3de50d7fa482e7548
Alex Goulart <goowlart@gmail.com>
2018-11-08 11:35:37 +0100

    solution of vulnerability problem

--- everest-app/server/database/index.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose'); //ele exporta do database que ja esta configurado
-
-mongoose.connect('mongodb://localhost/noderest', {
-  //useMongoClient: true
-}); // define a name for my database (in this case mine is called NODEREST)
-mongoose.Promise = global.Promise;
-
-module.exports = mongoose;
